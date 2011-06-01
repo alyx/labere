20cc097d2c07f9764a345245a5e445c3e55fb25f
pirogoeth <miyoko@maio.me>
2011-05-31 23:14:49 -0500

    added defer module to core, protocol model

---
+++ core/protocol.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import eventlet, os, time, datetime
+from eventlet.green import socket
+
+class Protocol(object):
+    """  protocol wrapper
+           protocol modules should use this model
+           to correctly handle events. """
+    
+    def __init__(self, conn):
+        """ initialize the connection objects
+            do NOT overwrite this """
+        
+        self.conn = conn
+        
+        # protocol options
+        
+        self.uid = False
+        self.euid = False
+        self.rserv = False
+        self.tburst = False
+        self.eopmod = False
+        self.mlock = False
+        
+        # default re
+        
+        self.pattern = "^(?:\:([^\s]+)\s)?([A-Za-z0-9]+)\s(?:([^\s\:]+)\s)?(?:\:?(.*))?$" 
+ 
+    def negotiate(self):
+        """ negotiate with the server
+              overwrite this function """
+ 
+        pass
+    
+    def parse(self, data):
+        """ parse incoming server data
+              overwrite this function """
+              
+        pass
