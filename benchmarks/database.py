0bc60ac2eea117b972b98ab1caaefcfc192ff433
pirogoeth <miyoko@maio.me>
2011-06-03 21:14:19 -0500

    updated benchmarks

--- benchmarks/database.py
+++ benchmarks/database.py
@@ -6,22 +6,61 @@ import benchmarking, time
      of database operations and time it, to see how long it will take. """
 
 operations = {
-    'load core': 'import core',
-    'load var and database': 'from core import var, database',
-    'initialize database': 'database.Database().close_db()',
-    'register a single user': 'database.Database().register_user("user", "password")',
-    'register two channels to user': 'database.Database().register_channel("#a", "password", "user"); database.Database().register_channel("#b", "password", "user")',
-    'modify metadata for user': 'database.Database().__refero__()["users"]["user"]["metadata"]["email"] = "someone@gmail.com"',
-    'register another channel': 'database.Database().register_channel("#c", "password", "user")',
-    'remove #c': 'database.Database().deregister_channel("#c")',
-    'deregister user': 'database.Database().deregister_user("user")',
     'sync and close the database': 'database.Database().close_db()'
 }
 
-def start():
-    for msg, operation in operations.iteritems():
-        benchmarking.start('%s' % (msg))
-        eval('operation')
-        benchmarking.stop('%s' % (msg))
+def run():
+    print 'generating 50,000 users...'
+    iusers = []
+    [iusers.append(i) for i in range(0, 50000, 1)]
+    print 'users generated...benchmarking.'
+    print 'loading modules..'
+    benchmarking.start('load modules')
+    import core
+    from core import var, database
+    benchmarking.stop('load modules')
+    print 'initializing db..'
+    benchmarking.start('init db')
+    db = database.Database()
+    benchmarking.stop('init db')
+    print 'registering a user..'
+    benchmarking.start('reg one user')
+    db.register_user('user', 'pasword')
+    benchmarking.stop('reg one user')
+    print 'modifying user metadata..'
+    benchmarking.start('modify user metadata')
+    db.__refero__()['users']['user']['metadata']['email'] = 'someone@example.com'
+    db.__refero__()['users']['user']['metadata']['soper'] = True
+    benchmarking.stop('modify user metadata')
+    print 'registering two channels..'
+    benchmarking.start('reg two chans')
+    db.register_channel('#a', 'password', 'user')
+    db.register_channel('#b', 'password', 'user')
+    benchmarking.stop('reg two chans')
+    print 'registering one channel..'
+    benchmarking.start('reg one chan')
+    db.register_channel('#c', 'password', 'user')
+    benchmarking.stop('reg one chan')
+    print 'deregistering one channel..'
+    benchmarking.start('dereg one chan')
+    db.deregister_channel('#c')
+    benchmarking.stop('dereg one chan')
+    print 'deregistering one user..'
+    benchmarking.start('dereg user')
+    db.deregister_user('user')
+    benchmarking.stop('dereg user')
+    print 'synching database..'
+    benchmarking.start('sync db')
+    db.sync_db()
+    benchmarking.stop('sync db')
+    print '[INTENSE] registering 50,000 users..'
+    benchmarking.start('register an insane amount of users')
+    [db.register_user('%s' % (user), 'password') for user in iusers]
+    benchmarking.stop('register an insane amount of users')
+    print 'closing database..'
+    benchmarking.start('close db')
+    db.close_db()
+    benchmarking.stop('close db')
+    print 'benchmarked.'
     time.sleep(2)
     benchmarking.print_all()
